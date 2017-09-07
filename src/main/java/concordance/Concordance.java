9b7763fbda43898deeb3e1fec801d27b0802b869
Simon Roberts <simon@dancingcloudservices.com>
2017-09-07 16:49:13 -0700

    Stream examples Concordance example

---
+++ src/main/java/concordance/Concordance.java
@@ -0,0 +1,32 @@
+package concordance;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Concordance {
+  private static final Pattern WORD_BOUNDARY = Pattern.compile("\\W+");
+  public static void main(String[] args) {
+    try (Stream<String> input = Files.lines(Paths.get("PrideAndPrejudice.txt"))) {
+      input.flatMap(s->WORD_BOUNDARY.splitAsStream(s))
+          .filter(s->s.length() > 0)
+          .map(s->s.toLowerCase())
+          .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+          .entrySet().stream()
+          .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+          .limit(200)
+          .forEach(e->System.out.printf("%20s : %5d\n", e.getKey(), e.getValue()));
+//          .forEach(s->System.out.println(s))
+          ;
+    } catch (IOException ex) {
+      Logger.getLogger(Concordance.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+}
--- src/main/java/students/StreamSchool.java
+++ src/main/java/students/StreamSchool.java
@@ -2,6 +2,7 @@ package students;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamSchool {
 
@@ -16,11 +17,35 @@ public class StreamSchool {
     school.add(Student.ofNameGpaCourses("Tony", 2.8F, "Math"));
 
     school.stream()
-        .filter(s->s.getGpa() > 3.3F)
-//        .map(s->s.getGpa())
-        .flatMap(s->s.getCourses().stream())
+        .filter(s -> s.getGpa() > 3.3F)
+        //        .map(s->s.getGpa())
+        .flatMap(s -> s.getCourses().stream())
         .distinct()
         .sorted()
-        .forEach(s->System.out.println("> " + s));
+        .forEach(s -> System.out.println("> " + s));
+    System.out.println("------------------------------");
+
+    school.stream()
+        .map(s -> s.getName() + ": " + s.getGpa())
+        .forEach(s -> System.out.println(s));
+
+    System.out.println("------------------------------");
+    school.stream()
+        .filter(s->s.getGpa() > 3.2)
+        .map(s -> s.getName() + ": " + s.getGpa())
+        .forEach(s -> System.out.println(s));
+
+    System.out.println("------------------------------");
+    school.stream()
+        .flatMap(s -> s.getCourses().stream().map(x->s.getName() + ": " + x))
+        .forEach(s -> System.out.println(s));
+
+    System.out.println("------------------------------");
+    Stream.iterate(1, x -> x + 2)
+        .peek(s->System.out.println("> " + s))
+        .skip(10_000)
+        .limit(100)
+        .forEach(s->System.out.println(s))
+        ;
   }
 }
